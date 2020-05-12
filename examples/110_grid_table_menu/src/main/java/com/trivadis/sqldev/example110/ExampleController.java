fd8f34ab48fe47268a1e3e0ba9e2b633083cd0f7
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-12 18:09:03 +0200

    simplified, handle three actions

--- examples/110_grid_table_menu/src/main/java/com/trivadis/sqldev/example110/ExampleController.java
+++ examples/110_grid_table_menu/src/main/java/com/trivadis/sqldev/example110/ExampleController.java
@@ -11,29 +11,17 @@ import oracle.ide.controller.IdeAction;
 
 public class ExampleController implements Controller {
 
-	private static ExampleController INSTANCE;
 	private static final Logger logger = Logger.getLogger(ExampleController.class.getName());
 	private static final int EXAMPLE_CONTEXT_MENU_1_CMD_ID = Ide.findOrCreateCmdID("EXAMPLE_CONTEXT_MENU_1_ACTION_ID");
 	private static final int EXAMPLE_CONTEXT_MENU_2_CMD_ID = Ide.findOrCreateCmdID("EXAMPLE_CONTEXT_MENU_2_ACTION_ID");
-	public static final IdeAction EXAMPLE_CONTEXT_MENU_1_ACTION = getAction(EXAMPLE_CONTEXT_MENU_1_CMD_ID);
-	public static final IdeAction EXAMPLE_CONTEXT_MENU_2_ACTION = getAction(EXAMPLE_CONTEXT_MENU_2_CMD_ID);
-
-	private static IdeAction getAction(int actionId) {
-		IdeAction action = IdeAction.get(actionId);
-		action.addController(getInstance());
-		return action;
-	}			
-
-	public static synchronized ExampleController getInstance() {
-		if (INSTANCE == null) {
-			logger.fine("initialize controller");
-			INSTANCE = new ExampleController();
-		}
-		return INSTANCE;
-	}		
+	private static final int EXAMPLE_CONTEXT_MENU_3_CMD_ID = Ide.findOrCreateCmdID("EXAMPLE_CONTEXT_MENU_3_ACTION_ID");
+	public static final IdeAction EXAMPLE_CONTEXT_MENU_1_ACTION = IdeAction.get(EXAMPLE_CONTEXT_MENU_1_CMD_ID);
+	public static final IdeAction EXAMPLE_CONTEXT_MENU_2_ACTION = IdeAction.get(EXAMPLE_CONTEXT_MENU_2_CMD_ID);
+	public static final IdeAction EXAMPLE_CONTEXT_MENU_3_ACTION = IdeAction.get(EXAMPLE_CONTEXT_MENU_3_CMD_ID);
 			
 	public ExampleController() {
 		super();
+		logger.fine("controller initialized.");
 	}
 	
 	private void show (String message) {
@@ -47,6 +35,8 @@ public class ExampleController implements Controller {
 			show("1");
 		} else if (cmdId == EXAMPLE_CONTEXT_MENU_2_CMD_ID) {
 			show("2");
+		} else if (cmdId == EXAMPLE_CONTEXT_MENU_3_CMD_ID) {
+			show("3");
 		} else {
 			return false;
 		}
