91988c9d531db6331c6b33a16dd340409e05bfed
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-12 18:43:09 +0200

    add menu listener as suggested by @bjeffrie
    see https://community.oracle.com/message/15615847#15615847 for details

--- examples/110_grid_table_menu/src/main/java/com/trivadis/sqldev/example110/ExampleAddin.java
+++ examples/110_grid_table_menu/src/main/java/com/trivadis/sqldev/example110/ExampleAddin.java
@@ -2,16 +2,49 @@ package com.trivadis.sqldev.example110;
 
 import java.util.logging.Logger;
 
+import javax.swing.JMenu;
+
+import oracle.dbtools.raptor.controls.grid.RaptorGridTable;
+import oracle.dbtools.raptor.controls.grid.contextmenu.GridContextMenuItem;
 import oracle.ide.Addin;
-import oracle.ide.editor.EditorManager;
+import oracle.ide.controller.ContextMenu;
 
 public class ExampleAddin implements Addin {
-	
+
 	private static final Logger logger = Logger.getLogger(ExampleAddin.class.getName());
 
 	@Override
 	public void initialize() {
-		EditorManager.getEditorManager().getContextMenu().addContextMenuListener(new ExampleMenuListener());
+		logger.fine("initializing...");
+		GridContextMenuItem menuListener = new GridContextMenuItem() {
+			/**
+			 * PRECONDITION: _table will NOT be null
+			 * 
+			 * @param contextMenu the context menu being built
+			 * @return true if createAndShowAction should be called
+			 */
+			@Override
+			protected boolean canShow(ContextMenu contextMenu) {
+				// and any other preconditions besides 'just' RaptorGridTable _table != null
+				return true;
+			}
+
+			/**
+			 * PRECONDITION: _table will NOT be null and canShow has returned true
+			 * 
+			 * @param contextMenu the context menu being built Add your menuItem(s) to the
+			 *                    context menu
+			 */
+			@Override
+			protected void createAndShowMenu(ContextMenu contextMenu) {
+				logger.fine("show context menu");
+				JMenu subMenu = contextMenu.createSubMenu(ExampleResources.getString("EXAMPLE_CONTEXT_SUBMENU_LABEL"),null,1,1);
+				subMenu.add(contextMenu.createMenuItem(ExampleController.EXAMPLE_CONTEXT_MENU_1_ACTION));
+				subMenu.add(contextMenu.createMenuItem(ExampleController.EXAMPLE_CONTEXT_MENU_2_ACTION));
+				contextMenu.add(subMenu);
+			}
+		};
+		RaptorGridTable.addGridContextMenu(menuListener);
 		logger.fine("addin initialized.");
 	}
 
