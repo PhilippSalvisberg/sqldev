9f702651df54ad353af966d65e7d50056d8e9e9a
Philipp Salvisberg <philipp.salvisberg@trivadis.com>
2020-05-14 02:51:05 +0200

    handle multi-selection: cells, rows, columns

--- examples/110_grid_table_menu/src/main/java/com/trivadis/sqldev/example110/ExampleController.java
+++ examples/110_grid_table_menu/src/main/java/com/trivadis/sqldev/example110/ExampleController.java
@@ -2,6 +2,7 @@ package com.trivadis.sqldev.example110;
 
 import java.awt.Component;
 import java.util.logging.Logger;
+import java.util.stream.IntStream;
 
 import javax.swing.JOptionPane;
 
@@ -29,39 +30,51 @@ public class ExampleController implements Controller {
 		logger.fine("controller initialized.");
 	}
 	
-	private static String getSelectedValue(RaptorGridTable tab) {
-		int selRow = tab.getSelectedRow();
-		int selCol = tab.getSelectedColumn();
-		String selectedValue = tab.getDisplayValueAt(selRow, selCol, -1);
-		return selectedValue;
+	private static String getSelectedValue(RaptorGridTable tab, boolean allValues) {
+		StringBuffer sb = new StringBuffer();
+		int[] rows = tab.getCellSelectionEnabled() || tab.getRowSelectionAllowed() ? tab.getSelectedRows()
+				: IntStream.range(0, tab.getRowCount()).toArray();
+		for (int selRow : rows) {
+			int[] cols = tab.getCellSelectionEnabled() || tab.getColumnSelectionAllowed() ? tab.getSelectedColumns()
+					: IntStream.range(0, tab.getColumnCount()).toArray();
+			for (int selCol : cols) {
+				if (allValues || sb.length() == 0) {
+					if (sb.length() > 0) {
+						sb.append(", ");
+					}
+					sb.append(tab.getDisplayValueAt(selRow, selCol, -1));
+				}
+			}
+		}
+		return sb.toString();
 	}
 	
-	private static String getSelectedValue(Component component) {
+	private static String getSelectedValue(Component component, boolean allValues) {
 		if (component instanceof RaptorGridTable) {
-			return getSelectedValue((RaptorGridTable)component);
+			return getSelectedValue((RaptorGridTable)component, allValues);
 		}
 		return null;
 	}
 
-	private void show (String message, Context context) {
+	private void show (String message, Context context, boolean allValues) {
 		View view = context.getView();
 		Component viewGui = view.getGUI();
 		String selectedValue = null;
 		if (viewGui instanceof RaptorGridTable) {
-			selectedValue = getSelectedValue(viewGui);
+			selectedValue = getSelectedValue(viewGui, allValues);
 		} else if (view instanceof XMLBasedEditor) {
 			if (ExampleContextMenu.getContextGridTable() != null) {
 				// parent or child table; the selected table is not available in the context
-				selectedValue = getSelectedValue(ExampleContextMenu.getContextGridTable());
+				selectedValue = getSelectedValue(ExampleContextMenu.getContextGridTable(), allValues);
 			} else {
-				// parent table only
-				selectedValue = getSelectedValue(((XMLBasedEditor)view).getMainUI().getUI());
+				// parent table only (dead code)
+				selectedValue = getSelectedValue(((XMLBasedEditor)view).getMainUI().getUI(), allValues);
 			}
 		} else if (view instanceof Worksheet) {
 			Worksheet worksheet = (Worksheet)view;
 			WorksheetResultPanel gui = worksheet.getSelectedResultPanel();
 			if (gui instanceof StatementRunnerResultsPanel) {
-				selectedValue = getSelectedValue(((StatementRunnerResultsPanel)gui).getResultSetTable());
+				selectedValue = getSelectedValue(((StatementRunnerResultsPanel)gui).getResultSetTable(), allValues);
 			}
 		}
 		JOptionPane.showMessageDialog(null, message + ": " + selectedValue, ExampleResources.getString("DIALOG_SHOW_TITLE"), JOptionPane.INFORMATION_MESSAGE);
@@ -71,9 +84,9 @@ public class ExampleController implements Controller {
 	public boolean handleEvent(IdeAction action, Context context) {
 		int cmdId = action.getCommandId();
 		if (cmdId == EXAMPLE_CONTEXT_MENU_1_CMD_ID) {
-			show("1", context);
+			show("1", context, false);
 		} else if (cmdId == EXAMPLE_CONTEXT_MENU_2_CMD_ID) {
-			show("2", context);
+			show("2", context, true);
 		} else {
 			return false;
 		}
